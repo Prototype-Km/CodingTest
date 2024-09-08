b94602407d3e0bec93edd7a99ed4549d92d52cb6
Prototype-Km <129861964+Prototype-Km@users.noreply.github.com>
2024-09-09 07:43:40 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 104 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main{
+	public static void main(String[] args)throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		
+		//9명 배열 
+		int[] arr = new int[9];
+		
+		//전체 난쟁이들의 키 총합 
+		int sum = 0;
+		
+		for(int i=0; i<9;i++) {
+			int n =	Integer.parseInt(br.readLine()); 
+			arr[i] = n; // 배열에 저장 
+			sum += arr[i]; 
+		}
+		
+		Arrays.sort(arr);
+		
+		int a =0 , b=0;
+		
+		for(int i=0; i<arr.length-1; i++) {
+			
+			for(int j=i+1; j<arr.length; j++) {
+				
+				if(sum - arr[i] -arr[j] == 100) {
+					a = i;
+					b = j;
+					break;
+				}
+			}
+		}
+		
+		for(int i=0;i<arr.length; i++) {
+			
+			if(i != a && i !=b) {
+				System.out.println(arr[i]);
+			}
+		}
+	}
+}
