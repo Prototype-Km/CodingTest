c8a33f1dee0b512e43e79a16e86b50010796b9b6
Prototype-Km <129861964+Prototype-Km@users.noreply.github.com>
2025-07-22 14:54:11 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 65.31 ms, Memory: 117 MB -BaekjoonHub

--- 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -1,25 +1,14 @@
 import java.util.*;
-import java.util.List;
-import java.util.ArrayList;
-
+import java.util.Stack;
 public class Solution {
     public int[] solution(int []arr) {
-        int[] answer = {};
+        Stack<Integer> stack = new Stack<>();
         
-        List<Integer> ar = new ArrayList<>();
-        //arr의 첫 번째 요소 담기 
-        ar.add(arr[0]); 
-
-        //arr 순회 
-        for(int i=1; i<arr.length;i++){
-            //arr의 현재 값이 직전 값과 같으면 무시 
-            if(arr[i-1] == arr[i]){
-                continue;
-            }else{
-                ar.add(arr[i]);
-            }
+        for(int i : arr){
+            if(stack.empty() || !stack.peek().equals(i)){
+                stack.push(i);
+            }       
         }
-        //list to array
-        return ar.stream().mapToInt(i->i).toArray();
+        return stack.stream().mapToInt(i -> i).toArray();
     }
 }
